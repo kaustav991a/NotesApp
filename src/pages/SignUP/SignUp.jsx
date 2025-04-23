6e7222a1690533f81dd80539f14996c1b5051b65
soumo365 <sumancena100@gmail.com>
2025-04-23 11:37:41 +0530

    todays push

--- src/pages/SignUP/SignUp.jsx
+++ src/pages/SignUP/SignUp.jsx
@@ -6,14 +6,14 @@ import "./SignUp.scss"
 import { Link } from 'react-router-dom'
 import { FaArrowLeft } from "react-icons/fa6";
 function SignUp() {
-      
-    const [user , setUser] = useState({
-         name: "",
-         email: "",
-         username : "",
-         password : "",
-         consfirmPassowrd: "",
-         isAccepted : false
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+        acceptedTerms: false
 
     })
 
@@ -28,25 +28,27 @@ function SignUp() {
                             <div className="auth-wrap">
                                 <Link to="/" className="nextBackBtn"> <FaArrowLeft /></Link>
                                 <h3 className='title-border'>Sign Up Your Account</h3>
-                                <form className='form-sec'>
-                                        <div className="inpt-wrp">
-                                        <input type="text" placeholder='Full Name' value={user.name} onChange={(e)=>{setUser({...user , name : e.target.value})}}/>
+                                <form className='form-sec' onSubmit={handleSubmit}>
+                                    <div className="inpt-wrp">
+                                        <input type="text" placeholder='Full Name' value={user.name} onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
                                     </div>
                                     <div className="inpt-wrp">
-                                        <input type="email" placeholder='Email' value={user.email} onChange={(e)=>{setUser({...user, email:e.target.value})}} />
+                                        <input type="email" placeholder='Email' value={user.email} onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
                                     </div>
                                     <div className="inpt-wrp">
-                                        <input type="text" placeholder='Username' value={user.username} onChange={(e)=>{setUser({...user , username : e.target.value})}} />
+                                        <input type="text" placeholder='Username' value={user.username} onChange={(e) => { setUser({ ...user, username: e.target.value }) }} />
                                     </div>
                                     <div className="inpt-wrp">
-                                        <input type="password" placeholder='Password' value={user.password}  onChange={(e)=>{setUser({...user , password : e.target.value})}}/>
+                                        <input type="password" placeholder='Password' value={user.password} onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
                                     </div>
                                     <div className="inpt-wrp">
-                                        <input type="password" placeholder='Confirm Password' />
+                                        <input type="password" placeholder='Confirm Password' value={user.confirmPassword} onChange={(e) => { setUser({ ...user, confirmPassword: e.target.value }) }} />
                                     </div>
-                                    <div className="form-check mb-3">
-                                        <input type="checkbox" class="form-check-input" id="validationFormCheck1"
-                                        />
+                                    <div class="form-check mb-3">
+                                        <input type="checkbox" class="form-check-input" id="validationFormCheck1" checked={user.acceptedTerms}
+                                            onChange={(e) =>
+                                                setUser({ ...user, acceptedTerms: e.target.checked })
+                                            } />
                                         <label class="form-check-label" for="validationFormCheck1">I accept the <a href="#">Terms and Conditions</a></label>
                                     </div>
                                     <div className="inpt-wrp submit">
@@ -58,7 +60,7 @@ function SignUp() {
                                 </form>
                             </div>
                         </Col>
-                       
+
                     </Row>
 
                 </Container>
