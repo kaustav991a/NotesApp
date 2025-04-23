8b6b762f2cdbe9fb4950b95f88b31499409d58eb
kaustav991a <kaustav.sengupta@fortmindz.in>
2025-04-23 13:10:42 +0530

    commit efore merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { RouterProvider } from "react-router-dom";
+import { UserProvider } from "./Context/UserContext"; // Import UserProvider
 
 import "./styles/base.scss";
 import router from "./Routes";
 
-// Wrapper to handle animation on route change
 function App() {
   return (
-    <RouterProvider router={router} />
-    // <LoadingProvider>
-    //   <AnimatePresence mode="wait">
-    //     <RouterProvider router={router} />
-    //   </AnimatePresence>
-    // </LoadingProvider>
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   );
 }
 
